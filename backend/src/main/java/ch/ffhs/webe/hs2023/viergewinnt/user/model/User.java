918802f12dc4e8ad3081ee9acde89a268093b3d6
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2023-10-25 18:00:11 +0200

    feat: add session entitiy

--- backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/user/model/User.java
+++ backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/user/model/User.java
@@ -1,17 +1,22 @@
 package ch.ffhs.webe.hs2023.viergewinnt.user.model;
 
 import ch.ffhs.webe.hs2023.viergewinnt.user.values.Role;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -34,4 +39,22 @@ public class User {
     private String password;
 
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private List<Session> sessions;
+
+    public void addSession(final String sessionId) {
+        final var session = Session.of(this, sessionId);
+        this.sessions.add(session);
+    }
+
+    public void removeSession(final String sessionId) {
+        this.sessions.removeIf(session -> session.getSessionId().equals(sessionId));
+    }
+
+    public List<Session> getSessions() {
+        return Collections.unmodifiableList(this.sessions);
+    }
 }
