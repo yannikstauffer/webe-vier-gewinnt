92e58a934cb11096517bab17c12a64c2abd3e718
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2023-10-25 18:05:05 +0200

    feat: add message service for simplified session management

--- backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/chat/ChatController.java
+++ backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/chat/ChatController.java
@@ -1,14 +1,19 @@
 package ch.ffhs.webe.hs2023.viergewinnt.chat;
 
+import ch.ffhs.webe.hs2023.viergewinnt.base.ErrorCode;
+import ch.ffhs.webe.hs2023.viergewinnt.base.VierGewinntException;
 import ch.ffhs.webe.hs2023.viergewinnt.chat.dto.InboundMessageDto;
 import ch.ffhs.webe.hs2023.viergewinnt.chat.dto.OutboundMessageDto;
 import ch.ffhs.webe.hs2023.viergewinnt.user.UserService;
+import ch.ffhs.webe.hs2023.viergewinnt.websocket.MessageService;
+import ch.ffhs.webe.hs2023.viergewinnt.websocket.values.MessageSources;
+import ch.ffhs.webe.hs2023.viergewinnt.websocket.values.Queues;
+import ch.ffhs.webe.hs2023.viergewinnt.websocket.values.Topics;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -18,12 +23,15 @@ import java.security.Principal;
 public class ChatController {
     private final ChatService chatService;
     private final UserService userService;
+    private final MessageService messageService;
 
     @Autowired
     public ChatController(final ChatService chatService,
-                          final UserService userService) {
+                          final UserService userService,
+                          final MessageService messageService) {
         this.chatService = chatService;
         this.userService = userService;
+        this.messageService = messageService;
     }
 
     @MessageMapping(MessageSources.MESSAGE)
@@ -35,13 +43,17 @@ public class ChatController {
         return OutboundMessageDto.of(stored);
     }
 
-
-    @MessageMapping("/private-message")
-    @SendToUser("/queue/chat")
-    public OutboundMessageDto receivePrivateMessage(@Payload final InboundMessageDto message, final Principal user) {
+    @MessageMapping(MessageSources.PRIVATE_MESSAGE)
+    public void receivePrivateMessage(@Payload final InboundMessageDto message, final Principal user) {
         final var sender = this.userService.getUserByEmail(user.getName());
 
         final var stored = this.chatService.storePrivateMessage(message, sender);
-        return OutboundMessageDto.of(stored);
+        final var receiver = stored.getReceiver()
+                .orElseThrow(() -> VierGewinntException.of(ErrorCode.USER_NOT_FOUND,
+                        "Message receiver for message with id " + stored.getId() + " not found"));
+
+        this.messageService.sendToUser(Queues.CHAT, receiver, OutboundMessageDto.of(stored));
+        this.messageService.sendToUser(Queues.CHAT, sender, OutboundMessageDto.of(stored));
     }
+
 }
