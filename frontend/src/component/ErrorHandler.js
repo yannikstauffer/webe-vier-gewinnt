4bfa4836909fcb16aa5844b4607fd82610d60970
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2023-10-28 14:10:51 +0200

    feat: added i18n handling

--- frontend/src/component/ErrorHandler.js
+++ frontend/src/component/ErrorHandler.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 import { useSubscription } from "react-stomp-hooks";
+import {useTranslation} from "react-i18next";
 
 const ErrorHandler = () => {
+  const { t, i18n } = useTranslation();
 
   const [errorState, setErrorState] = useState({
-     code: "", message: "",
+     code: "", messageKey: "",
   });
 
   const onErrorMessageReceived = (payload) => {
+
     console.log("error received", payload);
     let payloadData = JSON.parse(payload.body);
-    
-    setErrorState({ ...errorState, code: payloadData.code, message: payloadData.message })
+
+    setErrorState({ ...errorState, code: payloadData.code, messageKey: payloadData.messageKey })
   };
   useSubscription("/user/queue/error", onErrorMessageReceived);
 
   return errorState.code === "" ?
   (<></>) :
   (<div>
-    An error occured: {errorState.code} {errorState.message}
+    {t('error.label.anErrorOccurred')}: {t(errorState.messageKey)}
   </div>);
 }
 
