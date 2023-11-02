f28c607b014ecbe8eee361e46386eac296effb41
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2023-11-02 15:08:11 +0100

    test: add ChatServiceImplTest

--- backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/chat/ChatServiceImpl.java
+++ backend/src/main/java/ch/ffhs/webe/hs2023/viergewinnt/chat/ChatServiceImpl.java
@@ -1,5 +1,7 @@
 package ch.ffhs.webe.hs2023.viergewinnt.chat;
 
+import ch.ffhs.webe.hs2023.viergewinnt.base.ErrorCode;
+import ch.ffhs.webe.hs2023.viergewinnt.base.VierGewinntException;
 import ch.ffhs.webe.hs2023.viergewinnt.chat.dto.InboundMessageDto;
 import ch.ffhs.webe.hs2023.viergewinnt.chat.model.Message;
 import ch.ffhs.webe.hs2023.viergewinnt.chat.repository.MessageRepository;
@@ -25,17 +27,20 @@ class ChatServiceImpl implements ChatService {
 
     @Override
     public Message storePublicMessage(final InboundMessageDto inboundMessageDto, final User sender) {
+        this.validate(inboundMessageDto, MessageType.PUBLIC);
+
         final var message = Message.builder()
                 .text(inboundMessageDto.getText())
                 .messageType(MessageType.PUBLIC)
                 .sender(sender)
-                .sentAt(LocalDateTime.now())
                 .build();
         return this.messageRepository.save(message);
     }
 
     @Override
     public Message storePrivateMessage(final InboundMessageDto inboundMessageDto, final User sender) {
+        this.validate(inboundMessageDto, MessageType.PRIVATE);
+
         final var receiver = this.userService.getUserById(inboundMessageDto.getReceiverId());
 
         final var message = Message.builder()
@@ -43,7 +48,6 @@ class ChatServiceImpl implements ChatService {
                 .messageType(MessageType.PRIVATE)
                 .sender(sender)
                 .receiver(receiver)
-                .sentAt(LocalDateTime.now())
                 .build();
         return this.messageRepository.save(message);
     }
@@ -56,5 +60,22 @@ class ChatServiceImpl implements ChatService {
     @Override
     public List<Message> getPublicMessages() {
         return this.messageRepository.findPublicBy(LocalDateTime.now().minusMinutes(10));
+    }
+
+    void validate(final InboundMessageDto inboundMessageDto, final MessageType messageType) {
+        if (inboundMessageDto == null) {
+            throw new IllegalArgumentException("InboundMessageDto is null");
+        }
+        if (inboundMessageDto.getText() == null || inboundMessageDto.getText().isEmpty()) {
+            throw VierGewinntException.of(ErrorCode.MESSAGE_TEXT_EMPTY, "Message text is empty");
+        }
+
+        if (messageType == MessageType.PUBLIC && inboundMessageDto.getReceiverId() != null) {
+            throw VierGewinntException.of(ErrorCode.RECEIVER_ON_PUBLIC_MESSAGE, "Receiver should not be set on public message");
+        }
+
+        if (messageType == MessageType.PRIVATE && inboundMessageDto.getReceiverId() == null) {
+            throw VierGewinntException.of(ErrorCode.RECEIVER_NOT_SET, "Receiver not set");
+        }
     }
 }
