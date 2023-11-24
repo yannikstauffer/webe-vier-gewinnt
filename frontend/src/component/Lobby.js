65a8d31ad1cfac51e2d87948a354aadd26a40058
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2023-11-24 17:11:20 +0100

    feat: add basic style to lobby

--- frontend/src/component/Lobby.js
+++ frontend/src/component/Lobby.js
@@ -1,10 +1,13 @@
 import React, {useState, useRef} from 'react';
 import {useStompClient, useSubscription} from "react-stomp-hooks";
 import {useLocation, useNavigate} from 'react-router-dom';
+import {useTranslation} from "react-i18next";
+import './Lobby.css';
 
 const Lobby = ({userId}) => {
+    const {t} = useTranslation();
+
     const [games, setGames] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
     const lobbyGameTimeoutRef = useRef(null);
     const stompClient = useStompClient();
     const navigate = useNavigate();
@@ -54,7 +57,7 @@ const Lobby = ({userId}) => {
                     }
                 });
 
-                if ((updatedGame.userOne?.id === userId || updatedGame.userTwo?.id === userId) &&
+                if ((isCurrentUserPartOf(updatedGame)) &&
                     updatedGame.id === attemptingToJoinGameId) {
                     navigate(`/game/${updatedGame.id}`, {state: {prevPath: location.pathname}});
                     attemptingToJoinGameId = null;
@@ -66,6 +69,10 @@ const Lobby = ({userId}) => {
 
         }
     };
+
+    const isCurrentUserPartOf = (game) => {
+        return game.userOne?.id === userId || game.userTwo?.id === userId;
+    }
 
     useSubscription("/user/queue/games", onGamesReceived);
     useSubscription("/topic/lobby/games", onLobbyGameReceived);
@@ -95,14 +102,38 @@ const Lobby = ({userId}) => {
         }
     };
 
+    const getStateLabel = (game) => {
+        if (game.gameState === 'IN_PROGRESS') {
+            return t("game.state.inProgress");
+        } else if (game.gameState === 'PAUSED') {
+            return t("game.state.paused");
+        } else if (game.gameState === 'WAITING_FOR_PLAYERS') {
+            return t("game.state.wait");
+        } else if (game.gameState === 'FINISHED') {
+            return t("game.state.finished");
+        }
+    }
+
+    const isJoinDisabled = (game) => {
+        return game.gameState !== 'WAITING_FOR_PLAYERS'
+        && !(game.gameState === 'PAUSED' && isCurrentUserPartOf(game))
+    }
+
     return (
         <div>
             <h2>Lobby</h2>
             <button onClick={createGame}>Neues Spiel erstellen</button>
-            <ul>
+            <ul className="lobby-list">
                 {games.map((game) => (
-                    <li key={game.id} onClick={() => onGameClick(game.id)}>
-                        Spiel ID: {game.id} - Ersteller: {game.userOne?.firstName} - State: {game.gameState}
+                    <li className="lobby-game" key={game.id} onClick={() => onGameClick(game.id)}>
+                        <label>{t("game.title")} {game.id}</label><div>{getStateLabel(game)}</div>
+                        <label>{t("game.label.playerOne")}</label><div>{game.userOne?.firstName}</div>
+                        <label>{t("game.label.playerTwo")}</label><div>{game.userTwo?.firstName}</div>
+                        <button className="button join-button"
+                                onClick={() => onGameClick(game.id)}
+                                disabled={isJoinDisabled(game)}>
+                            {t("game.button.joinGame")}
+                        </button>
                     </li>
                 ))}
             </ul>
