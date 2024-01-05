2a280c038852c291b9ead1407f3515d871dfd466
Yannik Stauffer <yannik.stauffer@ti8m.ch>
2024-01-05 10:07:37 +0100

    fix: fix broken test

--- backend/src/test/java/ch/ffhs/webe/hs2023/viergewinnt/user/validation/PasswordMatchesValidatorTest.java
+++ backend/src/test/java/ch/ffhs/webe/hs2023/viergewinnt/user/validation/PasswordMatchesValidatorTest.java
@@ -1,10 +1,14 @@
 package ch.ffhs.webe.hs2023.viergewinnt.user.validation;
 
 import ch.ffhs.webe.hs2023.viergewinnt.user.dto.LoginDto;
+import jakarta.validation.ConstraintValidatorContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PasswordMatchesValidatorTest {
 
@@ -25,8 +29,16 @@ class PasswordMatchesValidatorTest {
 
     @Test
     void isValid_whenInvalid_returnsFalse() {
+        final var context = mock(ConstraintValidatorContext.class);
+        final var builder = mock(ConstraintValidatorContext.ConstraintViolationBuilder.class);
+        final var builderContext = mock(ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext.class);
+
+        when(context.getDefaultConstraintMessageTemplate()).thenReturn("baz");
+        when(context.buildConstraintViolationWithTemplate(any())).thenReturn(builder);
+        when(builder.addPropertyNode(any())).thenReturn(builderContext);
+
         final var dto = LoginDto.builder().password("foo").matchingPassword("bar").build();
-        final var result = this.passwordMatchesValidator.isValid(dto, null);
+        final var result = this.passwordMatchesValidator.isValid(dto, context);
         assertThat(result).isFalse();
 
     }
